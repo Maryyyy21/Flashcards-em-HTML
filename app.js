dfc46d170324a64c20d50b1eaa0ae121b9e3c62c
Maryane21 <maryane.fernandes.oliveira@escola.pr.gov.br>
2025-10-24 10:41:46 -0300

    Fim de samana

--- app.js
+++ app.js
@@ -3,3 +3,13 @@ function criaCartao(categoria, pergunta, resposra) {
     let cartao = document.createElement('article')
     cartao.className = 'cartao'
 }
+cartao.innerHTML =
+    <div class="cartao_conteudo">
+        <h3>${categoria}</h3>
+        <div_class />="cartao_conteudo_pergunta">
+        <p>${pergunta}</p>
+    </div>
+    <div_class"cartao_conteudo_resposta" >
+    <p>${resposta}</p>
+    </div >
+    </div >
